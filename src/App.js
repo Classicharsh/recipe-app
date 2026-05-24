5f429778ff848401bdae813832b21d0182cfefba
Harsh Prajapati <prajapatiharshit130@gmail.com>
2026-05-24 23:16:02 +0530

    Initial commit with glassmorphism UI & Edamam API v2 fix

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import Axios from "axios";
+import { useState } from "react";
+import "./app.css";
+import RecipeTile from "./components/recipe-tile";
+
+function App() {
+  const [query, setquery] = useState("");
+  const [recipes, setrecipes] = useState([]);
+
+  const YOUR_APP_ID = `82e453da`;
+  const YOUR_APP_KEY = "3bb5d1a3b992f408b9003effd74c9c22";
+
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+
+  const getRecipeInfo = async () => {
+    var result = await Axios.get(url);
+    setrecipes(result.data.hits);
+    console.log(result.data.hits);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipeInfo();
+  };
+
+  return (
+    <div className="app">
+      <h1 onClick={getRecipeInfo}>Food Recipe Plaza 🍔</h1>
+      <form className="app__searchForm" onSubmit={onSubmit}>
+        <input
+          className="app__input"
+          type="text"
+          placeholder="enter ingridient"
+          autoComplete="Off"
+          value={query}
+          onChange={(e) => setquery(e.target.value)}
+        />
+        <input className="app__submit" type="submit" value="Search" />
+      </form>
+
+      <div className="app__recipes">
+        {recipes.map((recipe) => {
+          return <RecipeTile key={recipe["recipe"]["uri"]} recipe={recipe} />;
+        })}
+      </div>
+
+      <footer className="app__footer">
+        <p>Made with ❤️ by Harsh Prajapati</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
